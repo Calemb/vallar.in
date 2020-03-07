81432f2419fadc8f2c445097f1cd642a456121b3
sculpt0r <calemb@gmail.com>
2020-03-07 17:08:07 +0100

    Setup sample next web page

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout';
+import Header from '../components/Header';
+export default function About () {
+  return (
+    <div>
+      <Layout>
+        <p>This is the about page</p>
+      </Layout>
+    </div>
+  );
+}
