f9d555097038d23cb846c9645d0dfd8b892e560f
sculpt0r <calemb@gmail.com>
2020-03-08 02:12:28 +0100

    Add logo and some css

--- components/Header.js
+++ components/Header.js
@@ -3,15 +3,30 @@ import Link from 'next/link';
 const linkStyle = {
   marginRight: 15
 };
+const logoStyle = {
+  border: '1px solid red'
+}
+const navStyle = {
+  border: '1px solid red'
+}
+const headerStyle = {
+  display: 'flex'
+}
 
 const Header = () => (
-  <div>
-    <Link href="/">
-      <a style={linkStyle}>Home</a>
-    </Link>
-    <Link href="/about">
-      <a style={linkStyle}>About</a>
-    </Link>
+  <div style={headerStyle}>
+    <img class="logo" src="/logo.png" alt="my image" style={logoStyle} />
+    <div class="nav" style={navStyle}>
+      <Link href="/">
+        <a style={linkStyle}>Home</a>
+      </Link>
+      <Link href="/about">
+        <a style={linkStyle}>About</a>
+      </Link>
+    </div>
+
+    <style jsx>{`
+    `} </style>
   </div>
 );
 
--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,22 @@
 import Header from './Header';
 
 const layoutStyle = {
-  margin: 20,
-  padding: 20,
-  border: '1px solid #DDD'
+  height: '100%',
+  width: '100vw',
+  border: '1px solid #DDD',
 };
 
 const Layout = props => (
   <div style={layoutStyle}>
     <Header />
     {props.children}
+    <style global jsx>{`
+        body {
+          margin: 0px;
+          padding: 0px;
+          background-color: #000;
+        }
+      `}</style>
   </div>
 );
 
